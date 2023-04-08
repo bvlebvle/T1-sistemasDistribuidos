375b6c2e05aeed8ef0f99046244cc72716dc8907
bvlebvle <valentina.diaz_go@mail.udp.cl>
2023-04-07 23:58:28 -0400

    cache a medias

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require("express");
 const responseTime = require("response-time");
 const axios = require("axios");
 const redis = require("redis");
-const client = redis.createClient({
-	host: 'localhost',
-	port: 6379
-});
 
 const app = express();
 
@@ -13,6 +9,10 @@ app.use(express.json());
 
 const fetch = require("node-fetch");
 
+const client = redis.createClient({
+	host: "localhost",
+	port: 6379
+});
 //solo hay 226 episodios
 // fech es una funcion que uno de la una url y nos da
 
@@ -21,14 +21,9 @@ let url_personajes = "https://api.sampleapis.com/simpsons/characters";
 
 //trae todos los episidios
 app.get("/episodes", async (req, res) => {
-	let fetchResponse = await axios.get(url_episodios);
-	client.set("episodes", JSON.stringify(fetchResponse.data),(err, reply) => {
-		if (err) {
-			console.log(err);
-		}
-		console.log(reply);
-	});
+	let fetchResponse = await fetch(url_episodios);
 
+	let parseJson = await fetchResponse.json();
 	res.json(parseJson);
 });
 
@@ -37,7 +32,9 @@ app.get("/episodes/:id", async (req, res) => {
 	let id = req.params.id;
 
 	let fetchResponse = await fetch(url_episodios + "/" + id);
+	console.log(fetchResponse);
 	let parseJson = await fetchResponse.json();
+	console.log(parseJson);
 	if (Object.keys(parseJson).length === 0) {
 		res.status(404).json({ message: "Episodio no encontrado" });
 	} else {
@@ -55,18 +52,30 @@ app.get("/characters", async (req, res) => {
 //trae un personaje por id
 app.get("/characters/:id", async (req, res) => {
 	let id = req.params.id;
+	let personaje_id = "characters/" + id;
 
-	let fetchResponse = await fetch(url_personajes + "/" + id);
-	let parseJson = await fetchResponse.json();
+	client.get(personaje_id, async (err, reply) => {
+		if (reply) {
+			console.log("cache");
+			return res.json(JSON.parse(reply));
+		}
 
-	if (Object.keys(parseJson).length === 0) {
-		res.status(404).json({ message: "Personaje no encontrado" });
-	} else {
-		res.json(parseJson);
-	}
+		let fetchResponse = await fetch(url_personajes + "/" + id);
+		let parseJson = await fetchResponse.json();
+
+		client.set(personaje_id, JSON.stringify(parseJson), (err, reply) => {
+			if (err) console.log(err);
+			console.log(reply);
+		});
+		if (Object.keys(parseJson).length === 0) {
+			res.status(404).json({ message: "Personaje no encontrado" });
+		} else {
+			res.json(parseJson);
+		}
+	});
 });
 
 //thunder client
 
-app.listen(3000);
-console.log("Server is running on port 3000");
+app.listen(4000);
+console.log("Server is running on port 4000");
